687a737ddaed4468f2be76f723143b1a29a79bf8
Robert Ferguson <robert.ferguson@digitalstrategy.ie>
2024-07-24 00:40:55 +0100

    moved app into its own file and imported into index and wrapped in strict mode for development purposes

---
+++ src/app.tsx
@@ -0,0 +1,47 @@
+import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
+import HomePage from "./pages/homePage";
+import MoviePage from "./pages/movieDetailsPage";
+import FavouriteMoviesPage from "./pages/favouriteMoviesPage";
+import MovieReviewPage from "./pages/movieReviewPage";
+import AddMovieReviewPage from './pages/addMovieReviewPage';
+import UpcomingPage from "./pages/upcomingPage";
+import SiteHeader from './components/siteHeader'
+import MoviesContextProvider from "./contexts/moviesContext";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { Auth } from "./components/authUser/auth";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 360000,
+      refetchInterval: 360000, 
+      refetchOnWindowFocus: false
+    },
+  },
+});
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <SiteHeader />
+        <Auth />
+        <MoviesContextProvider>
+          <Routes>
+            <Route path="/reviews/form" element={<AddMovieReviewPage/>} />
+            <Route path="/reviews/:id" element={<MovieReviewPage/>} />
+            <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
+            <Route path="/movies/:id" element={<MoviePage />} />
+            <Route path="/movies/upcoming" element={<UpcomingPage />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </MoviesContextProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,51 +1,9 @@
 import React from "react";
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router-dom";
-import HomePage from "./pages/homePage";
-import MoviePage from "./pages/movieDetailsPage";
-import FavouriteMoviesPage from "./pages/favouriteMoviesPage";
-import MovieReviewPage from "./pages/movieReviewPage";
-import AddMovieReviewPage from './pages/addMovieReviewPage';
-import UpcomingPage from "./pages/upcomingPage";
-import SiteHeader from './components/siteHeader'
-import MoviesContextProvider from "./contexts/moviesContext";
-import { QueryClientProvider, QueryClient } from "react-query";
-import { ReactQueryDevtools } from 'react-query/devtools';
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 360000,
-      refetchInterval: 360000, 
-      refetchOnWindowFocus: false
-    },
-  },
-});
-
-const App = () => {
-  return (
-  <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <SiteHeader />
-      <MoviesContextProvider>
-        <Routes>
-          <Route path="/reviews/form" element={<AddMovieReviewPage/>} />
-          <Route path="/reviews/:id" element={<MovieReviewPage/>} />
-          <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
-          <Route path="/movies/:id" element={<MoviePage />} />
-          <Route path="/movies/upcoming" element={<UpcomingPage />} />
-          <Route path="/" element={<HomePage />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </MoviesContextProvider>
-    </BrowserRouter>
-  <ReactQueryDevtools initialIsOpen={false} />
-  </QueryClientProvider>
-  );
-};
+import ReactDOM from 'react-dom/client';
+import App from './app';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
