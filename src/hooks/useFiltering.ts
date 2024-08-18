bea00c70a551a9e917711e3b7d28642bd312dc5f
Robert Ferguson <robert.ferguson@digitalstrategy.ie>
2024-08-18 15:07:48 +0100

    Default value if sortFilter?.value is undefined eslint ignore type any

--- src/hooks/useFiltering.ts
+++ src/hooks/useFiltering.ts
@@ -3,7 +3,9 @@ import { useState } from "react";
 interface Filter {
   name: string;
   value: string;
-  condition: (item: any, value: string) => boolean | any;
+  /* eslint-disable @typescript-eslint/no-explicit-any */
+  condition: (item: any, value: string) => boolean;
+  /* eslint-enable @typescript-eslint/no-explicit-any */
   type?: 'filter' | 'sort';
 }
 
@@ -18,15 +20,17 @@ const useFiltering = (filters: Filter[]) => {
   const filteringConditions = filters.filter(f => f.type !== 'sort').map((f) => f.condition);
   const sortingConditions = filters.filter(f => f.type === 'sort').map((f) => f.condition);
 
+  /* eslint-disable @typescript-eslint/no-explicit-any */
   const filterFunction = (collection: any) => {
     let filteredData = filteringConditions.reduce((data, conditionFn, index) => {
       return data.filter((item: any) => conditionFn(item, filterValues[index].value));
     }, collection);
+  /* eslint-enable @typescript-eslint/no-explicit-any */
 
     if (sortingConditions.length > 0) {
       filteredData = sortingConditions.reduce((data, conditionFn) => {
         const sortFilter = filterValues.find(filter => filter.name === "sort");
-        return conditionFn(data, sortFilter?.value);
+        return conditionFn(data, sortFilter?.value ?? "");
       }, filteredData);
     }
 
