751606687b3545fd664c1111d79e2e27bb8b3542
Robert Ferguson <robert.ferguson@digitalstrategy.ie>
2024-08-25 11:05:30 +0100

    removed filter from fantasy movies page

--- src/pages/fantasyMoviePage.tsx
+++ src/pages/fantasyMoviePage.tsx
@@ -2,36 +2,8 @@ import React, { useState } from "react";
 import FantasyTemplateMoviePage from '../components/fantasyTemplateMoviePage';
 import { FantasyMovieProps } from "../types/interfaces";
 import { fantasyMovieFirestoreStore } from "../models/fantasy-movie-firestore-store";
-import useFiltering from "../hooks/useFiltering";
-import { titleFilter, genreFilter, sortFilter } from "../filters";
-import MovieFilterUI from "../components/movieFilterUI";
 import { useQuery } from "@tanstack/react-query";
 import Spinner from "../components/spinner";
-
-const createFilters = () => {
-  const titleFiltering = {
-    name: "title",
-    value: "",
-    condition: titleFilter,
-    type: 'filter' as const,
-  };
-
-  const genreFiltering = {
-    name: "genre",
-    value: "0",
-    condition: genreFilter,
-    type: 'filter' as const,
-  };
-
-  const sortFiltering = {
-    name: "sort",
-    value: "name", // Set a default sort value
-    condition: sortFilter,
-    type: 'sort' as const,
-  };
-
-  return [titleFiltering, genreFiltering, sortFiltering];
-};
 
 const fetchAllFantasyMovies = async (): Promise<FantasyMovieProps[]> => {
   console.log("fetchAllFantasyMovies: Start fetching movies");
@@ -54,8 +26,6 @@ const AllFantasyMoviesPage: React.FC = () => {
     staleTime: 300000 // 5 minutes cache before data is considered stale
   });
 
-  const { filterValues, setFilterValues, filterFunction } = useFiltering(createFilters());
-
   console.log("AllFantasyMoviesPage: isLoading:", isLoading);
   console.log("AllFantasyMoviesPage: isError:", isError);
   console.log("AllFantasyMoviesPage: error:", error);
@@ -70,29 +40,13 @@ const AllFantasyMoviesPage: React.FC = () => {
     return <h1>{error.message}</h1>;
   }
 
-  const changeFilterValues = (type: string, value: string) => {
-    const changedFilter = { name: type, value: value };
-    const updatedFilterSet =
-      type === "title"
-        ? [changedFilter, filterValues[1]]
-        : [filterValues[0], changedFilter];
-    setFilterValues(updatedFilterSet);
-  };
-
   const movies = data ? data : [];
-  const displayedMovies = filterFunction(movies);
 
   return (
     <>
       <FantasyTemplateMoviePage
         title="Members Fantasy Movies"
-        movies={displayedMovies}
-      />
-      <MovieFilterUI
-        onFilterValuesChange={changeFilterValues}
-        titleFilter={filterValues[0].value}
-        genreFilter={filterValues[1].value}
-        sortOption={filterValues[2].value}
+        movies={movies}
       />
       <div>
         <span>Current Page: {page}</span>
